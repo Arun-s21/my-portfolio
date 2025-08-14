74505b4e3b161a2b36afc58ccfca6d4a28c1b382
Arun-s21 <arunsng18@gmail.com>
2025-08-14 19:37:38 +0530

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import { Analytics } from '@vercel/analytics/next';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body className={`${inter.className} bg-white`}>
         {children}
         <Footer/>
+        <Analytics />
+
       </body>
     </html>
   );
